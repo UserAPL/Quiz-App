47f746191e7dba265a8f4d16457d962ca054dffb
User/APL <arnapal.user@gmail.com>
2025-07-26 18:53:52 +0530

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,36 @@
+// data.js
+
+export const data = [
+  {
+    question: "Which city is called the City of Joy?",
+    option1: "Delhi",
+    option2: "Kolkata",
+    option3: "Mumbai",
+    option4: "Chennai",
+    ans: 2
+  },
+  {
+    question: "Which Indian dish is traditionally cooked in a clay oven called a tandoor?",
+    option1: "Biryani",
+    option2: "Butter Chicken",
+    option3: "Dosa",
+    option4: "Tandoori Chicken",
+    ans: 4
+  },
+  {
+    question: "Which sport is associated with the term 'sixer'?",
+    option1: "Baseball",
+    option2: "Tennis",
+    option3: "Cricket",
+    option4: "Hockey",
+    ans: 3
+  },
+  {
+    question: "Who wrote the famous novel 'The Jungle Book'?",
+    option1: "J.K. Rowling",
+    option2: "Rudyard Kipling",
+    option3: "Roald Dahl",
+    option4: "Mark Twain",
+    ans: 2
+  }
+];
